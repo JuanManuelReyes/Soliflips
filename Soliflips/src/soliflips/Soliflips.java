5b153fc1b6df51af7bba798c7189a7dcbe23d1bd
Juan Reyes <73147324+JuanManuelReyes@users.noreply.github.com>
2023-10-17 09:58:41 -0300

    Add files via upload

---
+++ Soliflips/src/soliflips/Soliflips.java
@@ -0,0 +1,78 @@
+/*
+Autores:
+Juan Manuel Reyes | Nro. Estudiante 316445
+Facundo Layes | Nro. Estudiante 248464
+
+Repositorio: https://github.com/JuanManuelReyes/Soliflips
+ */
+
+package soliflips;
+
+import java.util.*;
+
+/**
+ * La clase Main es el punto de entrada del juego Soliflips.
+ * Inicia y gestiona el ciclo principal del juego.
+ */
+public class Soliflips {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Juego juego = new Juego();
+        boolean juegoResuelto = false;
+        System.out.println("Bienvenido a Soliflips!");
+        System.out.println("Ingrese \'S\' para empezar a jugar u otro para salir.");
+        String respuesta = scanner.nextLine().toUpperCase();
+
+        if (respuesta.equals("S")) {
+            boolean opcionValida = false;
+            while (!opcionValida){
+                System.out.println("Elige una opcion (a, b, c): ");
+                System.out.println("a) Tomar datos del archivo \"datos.txt\"");
+                System.out.println("b) Usar el tablero predefinido");
+                System.out.println("c) Usar un tablero al azar");
+
+                String op = scanner.nextLine();
+                char opcion;
+                if (op.equals("")){
+                    continue;
+                }else{
+                    opcion = op.charAt(0);
+                }
+
+                // Iniciar los distintos tipos de juego
+                switch (opcion) {
+                    case 'a':
+                        juegoResuelto = juego.iniciar('a');
+                        opcionValida=true;
+                        break;
+                    case 'b':
+                        juegoResuelto = juego.iniciar('b');
+                        opcionValida=true;
+                        break;
+                    case 'c':
+                        juegoResuelto = juego.iniciar('c');
+                        opcionValida=true;
+                        break;
+                    default:
+                        System.out.println("Opcion no valida.");
+                        break;
+                }
+
+                // Verifica si el juego ha sido resuelto.
+                if (juegoResuelto) {
+                    System.out.println("Ingrese \'S\' para volver a jugar u otro para salir..");
+                    respuesta = scanner.nextLine().toUpperCase();
+                    if (respuesta.equals("S")) {
+                        opcionValida=false;
+                    }else{
+                        System.out.println("¡Hasta luego!");
+                        opcionValida=true;
+                    }
+                }
+            }
+        }else{
+            System.out.println("¡Hasta luego!");
+        }
+    }
+}
